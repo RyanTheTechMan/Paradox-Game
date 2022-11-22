7629df9e879d65e6a41eb1bcfb9186bba0b4dcab
Harris Fort <harrisfort99@gmail.com>
2022-11-22 13:38:44 -0500

    Cursor lock script for testing

---
+++ Assets/__Scripts/CursorController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// There is a Unity bug where it does not lock until you click lol
+public class CursorController : MonoBehaviour {
+    public bool lockedByDefault = true;
+    private bool isLocked;
+    
+    void Start() {
+        isLocked = lockedByDefault;
+        if (lockedByDefault) {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    void Update() {
+        if (Input.GetKey(KeyCode.Escape)) {
+            Cursor.lockState = isLocked ? CursorLockMode.None : CursorLockMode.Locked;
+            isLocked = !isLocked;
+        }
+    }
+}
