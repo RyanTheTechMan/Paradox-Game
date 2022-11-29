6dd232e44368cdeb9dc81a5b5d85df13c25e0b56
Harris Fort <harrisfort99@gmail.com>
2022-11-29 13:28:20 -0500

    Use RigidBody movement instead of Character Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public GameObject playerCamera;
-    private CharacterController controller;
     
     public float playerSpeed = 2.0f;
-    public float jumpHeight = 1.0f;
-    public float gravityValue = -9.81f;
-    
+    public float jumpForce = 50.0f;
+    // public float jumpHeight = 1.0f;
+    // public float gravityValue = -9.81f;
+
+    private Rigidbody rigidbody;
     private Vector3 playerVelocity;
     private bool isGrounded;
 
     private float horizontalInput;
     private float verticalInput;
-    
-    void Start() {
-        controller = GetComponent<CharacterController>();
-    }
 
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
     void Update() {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         
-        isGrounded = controller.isGrounded;
-        if (isGrounded && playerVelocity.y < 0) {
-            playerVelocity.y = 0f;
-        }
-
         Vector3 forward = playerCamera.transform.forward;
         Vector3 right = playerCamera.transform.right;
 
@@ -42,11 +38,10 @@ public class PlayerController : MonoBehaviour {
         Vector3 moveDirection = (forward * verticalInput) + (right * horizontalInput);
         playerVelocity = moveDirection * playerSpeed;
 
-        if (Input.GetButtonDown("Jump") && isGrounded) {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        if (Input.GetButtonDown("Jump")) {
+            rigidbody.AddForce(Vector3.up * jumpForce);
         }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        transform.Translate(playerVelocity * Time.deltaTime);
     }
 }
