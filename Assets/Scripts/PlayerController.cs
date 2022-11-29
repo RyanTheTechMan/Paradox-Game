95723c82700a05aac677f25b07d813bcacc70e10
Ryan Ernest <ryan@rivergroup.com>
2022-11-29 14:22:50 -0500

    Added Input Manager and Camera Controls and fixed movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,8 @@ using UnityEngine;
 // https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
 
 public class PlayerController : MonoBehaviour {
-    public GameObject playerCamera;
+    public Camera cameraLeft;
+    public Camera cameraRight;
     
     public float playerSpeed = 2.0f;
     public float jumpForce = 50.0f;
@@ -16,19 +18,42 @@ public class PlayerController : MonoBehaviour {
     private Vector3 playerVelocity;
     private bool isGrounded;
 
-    private float horizontalInput;
-    private float verticalInput;
+    private PlayerControls controls;
 
-    void Start() {
+    private void OnEnable() {
+        controls.Enable();
+    }
+    
+    private void OnDisable() {
+        controls.Disable();
+    }
+
+    void Awake() {
         rigidbody = GetComponent<Rigidbody>();
+        controls = new PlayerControls();
+    }
+
+    private void Start() {
+        controls.FirstPerson.Jump.performed += ctx => DoJump();
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
     
     void Update() {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        DoPlayerMove();
+    }
+
+    private void FixedUpdate() {
+        DoCameraMove();
+    }
+
+    private void DoPlayerMove() {
+        float horizontalInput = controls.FirstPerson.Move.ReadValue<Vector2>().x;
+        float verticalInput = controls.FirstPerson.Move.ReadValue<Vector2>().y;
         
-        Vector3 forward = playerCamera.transform.forward;
-        Vector3 right = playerCamera.transform.right;
+        Vector3 forward = cameraLeft.transform.forward;
+        Vector3 right = cameraLeft.transform.right;
 
         forward.y = 0;
         right.y = 0;
@@ -38,10 +63,18 @@ public class PlayerController : MonoBehaviour {
         Vector3 moveDirection = (forward * verticalInput) + (right * horizontalInput);
         playerVelocity = moveDirection * playerSpeed;
 
-        if (Input.GetButtonDown("Jump")) {
-            rigidbody.AddForce(Vector3.up * jumpForce);
-        }
-
         transform.Translate(playerVelocity * Time.deltaTime);
+    }
+    
+    private void DoCameraMove() {
+        Vector2 mouseDelta = controls.FirstPerson.Look.ReadValue<Vector2>();
+        
+        cameraLeft.transform.rotation *= Quaternion.AngleAxis(mouseDelta.x * Time.deltaTime * 30, Vector3.up);
+        cameraLeft.transform.rotation *= Quaternion.AngleAxis(mouseDelta.y * Time.deltaTime * 30, Vector3.left);
+        cameraLeft.transform.rotation = Quaternion.Euler(cameraLeft.transform.rotation.eulerAngles.x, cameraLeft.transform.rotation.eulerAngles.y, 0);
+    }
+
+    private void DoJump() {
+        rigidbody.AddForce(Vector3.up * jumpForce);
     }
 }
