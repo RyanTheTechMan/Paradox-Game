74bba6285a1135e1e8be2d9b685bd40285d7e12f
Ryan Ernest <ryan@rivergroup.com>
2022-12-02 17:51:35 -0500

    Added ResolutionChangeEvent

---
+++ Assets/__Scripts/ResolutionChangeEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+    
+    private Vector2 _screenSize; // Used for testing OnResolutionChange
+    delegate void OnResolutionChanged();
+    OnResolutionChanged onResolutionChanged;
+    [NonSerialized]
+    public bool resolutionChanging = false;
+    public float timeSinceLastChange 
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(gameObject);
+        
+        
+        _screenSize.x = Screen.width;
+        _screenSize.y = Screen.height;
+    }
+    
+    
+    private void Update()
+    {
+        // When the resolution changes, call OnResolutionChanged.
+        if (_screenSize.x != Screen.width || _screenSize.y != Screen.height)
+        {
+            resolutionChanging = true;
+            onResolutionChanged?.Invoke();
+        }
+        
+
+        _screenSize.x = Screen.width;
+        _screenSize.y = Screen.height;
+    }
+}
