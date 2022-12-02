06607190a69a16de1e1fbf0b62b473f3abff7bc9
Ryan Ernest <ryan@rivergroup.com>
2022-12-02 17:53:16 -0500

    ResolutionChangeEvent Fully working

--- Assets/__Scripts/ResolutionChangeEvent.cs
+++ Assets/__Scripts/ResolutionChangeEvent.cs
@@ -1,36 +1,67 @@
 ﻿using System;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
-    public static GameManager instance;
+public class ResolutionChangeEvent : MonoBehaviour {
+    public static event Action onResolutionChangedStarted;
+    public static event Action onResolutionChangedEnded;
     
-    private Vector2 _screenSize; // Used for testing OnResolutionChange
-    delegate void OnResolutionChanged();
-    OnResolutionChanged onResolutionChanged;
-    [NonSerialized]
-    public bool resolutionChanging = false;
-    public float timeSinceLastChange 
+    private static ResolutionChangeEvent instance;
+    private static bool resolutionChanging;
+    private static Vector2 _screenSize; // Used for testing OnResolutionChange
+    private static float _timeSinceLastChange = 0f;
+    private const float _waitTime = 0.5f; // Wait time before resolution change is completed.
+
+    public GameObject textOverlay;
+    private GameObject _textOverlay;
 
     private void Awake()
     {
-        if (instance == null) instance = this;
-        else Destroy(gameObject);
-        
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.LogWarning("There can only be one ResolutionChangeEvent in the scene.");
+            Destroy(gameObject);
+        }
         _screenSize.x = Screen.width;
         _screenSize.y = Screen.height;
     }
-    
-    
+
     private void Update()
     {
         // When the resolution changes, call OnResolutionChanged.
         if (_screenSize.x != Screen.width || _screenSize.y != Screen.height)
         {
-            resolutionChanging = true;
-            onResolutionChanged?.Invoke();
+            if (!resolutionChanging)
+            {
+                resolutionChanging = true;
+                onResolutionChangedStarted?.Invoke();
+                _textOverlay ??= Instantiate(textOverlay, transform);
+            }
+            _timeSinceLastChange = 0f;
         }
-        
+        else
+        {
+            if (resolutionChanging)
+            {
+                if (_timeSinceLastChange >= _waitTime)
+                {
+                    resolutionChanging = false;
+                    _timeSinceLastChange = 0f;
+                    onResolutionChangedEnded?.Invoke();
+                    if (_textOverlay != null)
+                    {
+                        Destroy(_textOverlay);
+                        _textOverlay = null;
+                    }
+                }
+            }
+            
+            _timeSinceLastChange += Time.deltaTime;
+        }
 
         _screenSize.x = Screen.width;
         _screenSize.y = Screen.height;
