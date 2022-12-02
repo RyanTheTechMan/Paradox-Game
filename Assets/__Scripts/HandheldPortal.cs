86b1dd36127690260aa351e0af8b30120443abee
Ryan Ernest <ryan@rivergroup.com>
2022-12-02 17:54:37 -0500

    Started work on handheld portal

---
+++ Assets/__Scripts/HandheldPortal.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandheldPortal : MonoBehaviour
+{
+    private Camera _camera;
+    
+    private RenderTexture _renderTexture;
+    public RawImage cameraView;
+
+    private PlayerController _player;
+
+    private void OnEnable()
+    {
+        ResolutionChangeEvent.onResolutionChangedEnded += Awake;
+        _camera.enabled = true;
+    }
+    
+    
+    private void OnDisable()
+    {
+        ResolutionChangeEvent.onResolutionChangedEnded -= Awake;
+        _camera.enabled = true;
+    }
+
+    private void Awake()
+    {
+        _camera = GetComponentInChildren<Camera>();
+        
+        if (_renderTexture != null) _renderTexture.Release();
+        _renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        _renderTexture.Create();
+        
+        _player ??= GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+
+        _camera.targetTexture = _renderTexture;
+        cameraView.texture = _renderTexture;
+    }
+    
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _camera.transform.position = _player._camera.transform.position;
+        _camera.transform.rotation = _player._camera.transform.rotation;
+    }
+}
