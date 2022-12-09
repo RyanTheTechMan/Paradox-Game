4efc4846f47bae7f91577d106c077e99da440a38
Ryan Ernest <ryan@rivergroup.com>
2022-12-09 15:49:26 -0500

    Cleaned up Door animation

--- Assets/__Scripts/InteractableControllers/DoorObject.cs
+++ Assets/__Scripts/InteractableControllers/DoorObject.cs
@@ -2,16 +2,16 @@
 using UnityEngine;
 
 public class DoorObject : ActivatableObject {
-    private bool _animating;
-    private GameObject _leftDoor;
-    private GameObject _rightDoor;
+    private Transform _leftDoor;
+    private Transform _rightDoor;
+    
     private float _restingPos;
     private float _moveDistance;
     
     protected override void Awake() {
         base.Awake();
-        _leftDoor = transform.GetChild(0).gameObject;
-        _rightDoor = transform.GetChild(1).gameObject;
+        _leftDoor = transform.GetChild(0).gameObject.transform;
+        _rightDoor = transform.GetChild(1).gameObject.transform;
         _restingPos = _leftDoor.transform.localPosition.x;
         _moveDistance = _leftDoor.GetComponent<BoxCollider>().size.x * 0.95f;
     }
@@ -21,16 +21,21 @@ public class DoorObject : ActivatableObject {
     }
     
     private IEnumerator DoAnimate() {
-        float startPos = _leftDoor.transform.localPosition.x;
-        float endPos = IsActive ? Mathf.Abs(_restingPos) + _moveDistance : _restingPos;
-        float speed = Mathf.Abs(startPos - endPos);
+        const float speed = 2f;
+        Vector3 pos = _leftDoor.localPosition;
         
-        var t = 0f;
+        float startPos = pos.x;
+        float endPos = IsActive ? Mathf.Abs(_restingPos) + _moveDistance : _restingPos;
+        float speedOffset = Mathf.Abs(startPos - endPos);
+        speedOffset = speed * (1 / speedOffset);
+        
+        float t = 0f;
         bool animateState = IsActive;
         while (t < 1f && animateState == IsActive) {
-            t += Time.deltaTime * 2f * (1 / speed);
-            _leftDoor.transform.localPosition = new Vector3(Mathf.Lerp(startPos, endPos, t), 0, -0.5f);
-            _rightDoor.transform.localPosition = new Vector3(Mathf.Lerp(-startPos, -endPos, t), 0, 0.5f);
+            t += Time.deltaTime * speedOffset;
+            pos.x = Mathf.Lerp(startPos, endPos, t);
+            _leftDoor.localPosition = pos;
+            _rightDoor.localPosition = -pos;
             yield return null;
         }
     }
