91ae40e980cf86683a140877505bd05eff30d425
Ryan Ernest <ryan@rivergroup.com>
2022-12-10 19:26:17 -0500

    PRE-UPDATE: Started working on LevelManager

--- Assets/__Scripts/LevelManager.cs
+++ Assets/__Scripts/LevelManager.cs
@@ -4,11 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
+    public static LevelManager Instance { get; private set; }
+    public static event Action OnLevelLoad;
+    public static event Action OnLevelComplete;
+
+    public Vector3 PlayerSpawnPosition;
+
+    [SerializeField] private GameObject levelLoadRoom;
+    
+    public void Awake() {
+        if (Instance == null) Instance = this;
+        else {
+            Destroy(gameObject);
+            Debug.LogWarning("LevelManager already exists. Destroying duplicate.");
+        }
+        OnLevelLoad?.Invoke();
+    }
+
+    public void OnDestroy() {
+        
+    }
+
     public void OnLevelEnd() {
         Debug.LogWarning("Number 1 victory royale");
     }
-
-    public void Awake() {
-        LevelGoalHandler.OnLevelComplete += OnLevelEnd;
-    }
+    
+    
 }
